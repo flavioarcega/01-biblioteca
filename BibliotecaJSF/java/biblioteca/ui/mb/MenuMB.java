8b05dd0bbfae8c8aedeafd8fb809bffe90f44ae5
flaviocasas <flaviocasas@f41b8d13-06bb-6195-f7ef-fcd283c654c2>
2010-12-01 14:49:41 +0000

    Implementado login através de PhaseListener.

---
+++ BibliotecaJSF/java/biblioteca/ui/mb/MenuMB.java
@@ -0,0 +1,43 @@
+package biblioteca.ui.mb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import biblioteca.bc.UsuarioBC;
+import biblioteca.persistence.entity.Usuario;
+
+@ManagedBean
+@SessionScoped
+public class MenuMB {
+	private String    login;
+	private String    senha;
+	private Usuario   usuario;
+	private UsuarioBC usuarioBC;
+	
+	public MenuMB() {
+		usuarioBC = new UsuarioBC();
+	}
+	
+	public String getNavigationRule(String navigationEnum) {
+		return NavigationEnum.valueOf(navigationEnum).getNavigationRule();
+	}
+	public Usuario getUsuario() {
+		return this.usuario;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Object login() {
+		usuario = usuarioBC.pesquisarPorLogin(login);
+		return null;
+	}
+}
