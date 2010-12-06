25db9750651489269c0bb98ed60447b35750e2bb
flaviocasas@gmail.com <flaviocasas@gmail.com@f41b8d13-06bb-6195-f7ef-fcd283c654c2>
2010-12-06 02:16:00 +0000

    Alterações 04 e 05/12

--- BibliotecaJSF/java/biblioteca/persistence/dao/LivroDAO.java
+++ BibliotecaJSF/java/biblioteca/persistence/dao/LivroDAO.java
@@ -1,5 +1,10 @@
 package biblioteca.persistence.dao;
 
+
+import java.util.List;
+
+import javax.persistence.Query;
+
 import biblioteca.persistence.entity.Livro;
 
 public class LivroDAO extends AbstractDAO<Livro> {
@@ -8,4 +13,15 @@ public class LivroDAO extends AbstractDAO<Livro> {
 	public LivroDAO() {
 		super(Livro.class);
 	}
+	
+	public Livro findLivroByExemplar(Integer exemplar) {
+		Query byExemplarQuery = this.getEntityManager().createQuery("select object(c) from " + this.getEntityClassName() + " as c where c.id = ?1" );
+		byExemplarQuery.setParameter(1, exemplar);
+		List<Livro> lista= byExemplarQuery.getResultList();
+		if (lista.size()==0)
+			return null;
+		else
+			return lista.get(0);
+	}
+
 }
